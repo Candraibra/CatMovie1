5160b0c552dd43975615de868a18b55110194d17
Candraibra <canisanie@gmail.com>
2019-10-24 07:52:50 +0700

    add detail activity

---
+++ app/src/main/java/com/candraibra/catmovie/ui/activity/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.candraibra.catmovie.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.candraibra.catmovie.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/com/candraibra/catmovie/ui/activity/detail/DetailMovieActivity.java
+++
@@ -1,4 +0,0 @@
-package com.candraibra.catmovie.ui.activity.detail;
-
-public class DetailMovieActivity {
-}
